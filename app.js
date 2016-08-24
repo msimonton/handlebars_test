f7300ef4dd9c711d8d56412e5378334cdc3e86fe
Marc Simonton <marcasimonton@gmail.com>
2016-08-24 11:27:26 -0600

    locals-and-ish

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const express = require('express');
-
 let app = express();
+var hbs = require('hbs');
+const locals = require('./locals.js');
+
+
+app.set('view engine', 'hbs');
+hbs.localsAsTemplateData(app);
+
+locals(app)
+
+
 
 app.get('/', (req,res)=> {
-  res.json({message: 'Hello from express'});
-
+  res.render('home',  {
+    foods: [
+      'chalupa',
+      'ceviche',
+      'candy sprinkles'
+    ]
+  });
 });
+
 
 app.get('/page2', (req,res)=> {
-  res.json({ message: 'I am page 2!'})
+  res.render('page2')
 });
+
 
 app.listen(4000, ()=>  {
   console.log('Server running on port 4000');
---
+++ locals.js
@@ -0,0 +1,6 @@
+function locals(app)  {
+  app.locals.cohortName = 'g31';
+  app.locals.title = 'My handlebars exapmle'
+}
+
+module.exports= locals;
